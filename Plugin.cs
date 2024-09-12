363c5351c381df74cd354b643a64703bb3fc56c7
Mystikal91 <mystikal91@gmail.com>
2024-09-12 23:54:10 +0200

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,124 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using System;
+using UnityEngine; 
+
+namespace mystikal.dinkum.DurabilityManager
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+
+        public static float _ToolDurabilityMultiplier;
+        public static float _PowerToolDurabilityMultiplier;
+        public static float _WeaponDurabilityMultiplier;
+        public static int _FirstAidKitUses;
+
+        private void Awake()
+        {
+            // Set global plugin logger
+            Plugin.Log = base.Logger;
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            // Config
+            Config.Bind("!Developer",                                       // The section under which the option is shown
+                    "NexusID",                                              // The key of the configuration option in the configuration file
+                    350,                                                    // The default value
+                    "Nexus Mod ID");                                        // Description of the option to show in the config file
+
+            _ToolDurabilityMultiplier = Mathf.Clamp(
+                                                Config.Bind<float>(
+                                                    "General",             
+                                                    "ToolDurabilityMultiplier", 
+                                                    1.0f,                    
+                                                    "The multiplier amount for Tools (not Power Tools). Minimum value is 0.01, Max is 1000.0")
+                                                .Value,
+                                                0.01f,
+                                                1000.0f);
+            _PowerToolDurabilityMultiplier = Mathf.Clamp(
+                                                Config.Bind<float>(
+                                                    "General",
+                                                    "PowerToolDurabilityMultiplier",
+                                                    1.0f,
+                                                    "The multiplier amount for Power Tools. Minimum value is 0.01, Max is 1000.0")
+                                                .Value, 
+                                                0.01f, 
+                                                1000.0f);
+            _WeaponDurabilityMultiplier = Mathf.Clamp(
+                                                Config.Bind<float>(
+                                                    "General",
+                                                    "WeaponDurabilityMultiplier",
+                                                    1.0f,
+                                                    "The multiplier amount for Tools. Minimum value is 0.01, Max is 1000.0")
+                                                .Value,
+                                                0.01f,
+                                                1000.0f);
+            _FirstAidKitUses = Mathf.Clamp(
+                                        Config.Bind<int>(
+                                            "General",
+                                            "FirstAidKitUses",
+                                            3,
+                                            "Change the amount of time the First Aid Kit can be used. This is the AMOUNT value, not a MULTIPLIER. Minimum value is 1, Max is 10000")
+                                        .Value,
+                                        1,
+                                        10000);
+        }
+
+        private void Start()
+        {
+
+            foreach (InventoryItem current in Inventory.Instance.allItems)
+            {
+                if (!current)
+                    continue; // Let's just prevent NPE if it's null. It shouldn't happen, but better safe than sorry
+
+                if (!current.hasFuel)   // If the current Item doesn't use fuel, let's just keep going
+                    continue;
+
+                bool flag = false;
+                // Let's handle some items individually first
+                switch (current.getItemId())
+                {
+                    case 1096:  // Bottle of clouds
+                    case 1444:  // Cup of Sunshine
+                    case 1530:  // Hand Trolley
+                        // This items count as tools, but we don't want to edit them. We'll just skip them
+                        flag = true;
+                        break;
+                    case 119: // ItemId 119 is First Aid Kit. For this one we replace the max fuel with the amount set by the user
+                        current.fuelMax = _FirstAidKitUses;
+                        flag = true;
+                        break;
+                    case 5:     // Shovel of Dirt
+                    case 704:   // Shovel of Sand
+                    case 705:   // Shovel of Mud
+                    case 706:   // Shovel of Red Sand
+                        // This items do not count as tools, but we need to have the same durability of a shovel
+                        current.fuelMax = Convert.ToInt32(current.fuelMax * _ToolDurabilityMultiplier);
+                        flag = true;
+                        break;
+                }
+
+                
+                if ((flag) ||               // If we handled the item in the switch-case...
+                    (!current.isATool) ||   // ... or If this isn't a tool ...
+                    (current.fuelMax == 0)) // ... or If it's an "empty" tool ....
+                    continue;               // ... let's just continue to the next item
+
+                // Power Tools
+                if (current.isPowerTool)
+                    current.fuelMax = Convert.ToInt32(current.fuelMax * _PowerToolDurabilityMultiplier);
+                // Weapons
+                else if (current.staminaTypeUse == InventoryItem.staminaType.Hunting)
+                    current.fuelMax = Convert.ToInt32(current.fuelMax * _WeaponDurabilityMultiplier);
+                // Simple tools, not power tools
+                else 
+                    current.fuelMax = Convert.ToInt32(current.fuelMax * _ToolDurabilityMultiplier);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Dinkum-DurabilityManager
-Allow the player to change the durability of items
+
+Nexus Mod Link: <https://www.nexusmods.com/dinkum/mods/350>  
+A simple BepInEx mod for Dinkum that allow to change the durability of items and First Aid Kits
+
+## Installation
+
+1. Install [BepInEx 6.0.0-pre.1](https://discord.com/channels/892654052989628436/1060375232642306088/1060375232642306088) using the tool and run the game once
+2. Download and Install "DurabilityManager" using Vortex or by downloading "mystikal.dinkum.DurabilityManager.dll" from Nexus Files or GitHub and pasting it into "BepInEx\plugins\" folder
+3. Run the game again, it will create the file "BepInEx\config\mystikal.dinkum.DurabilityManager.cfg"
+4. Open this file with a text editor and change the values according to the next chapter. **If you don't change those values the game will work the same as without the mod**
+
+## Settings
+
+Inside the config file at BepInEx\config\mystikal.dinkum.DurabilityManager.cfg you will find NexusID (leave this value as is) and 4 values
+* ToolDurabilityMultiplier
+* PowerToolDurabilityMultiplier
+* WeaponDurabilityMultiplier
+* FirstAidKitUses
+
+The first 3 values are multiplier for Tools, Power Tools and Weapons durability. The items base durability will be multiplied with the respective value. You can set those variables to any values from 0.01 to 1000.0
+If you want to double the tools durability, for example, set ToolDurabilityMultiplier to 2
+You can use decimal values too. Just set those values to something like 2.75
+
+FirstAidKitUses will change how many times a First Aid Kit can be used. This is an absolute value, not a multiplier. For example, just set it to 10 if you want to use it 10 times before breaking 
+
+## Disclaimer
+
+Every item that have a "usage meter" (power tools, tools, weapons...) basically have 3 values that matters. A "maxFuel", a "fuelOnUse" and the current "fuel" . We can't really work on fuelOnUse, due to the fact that it is an integer and most of the time is 1, so we can't really play with it. We can, however, edit the "maxFuel" value. For example, if we just double this value, the item can be used twice as much. 
+
+Due to this fact, once you'll install the mod and set, for example, the maxFuel for PowerTools to 2 times the original value (PowerToolDurabilityMultiplier = 2.0), every Power Tool you had in your inventory with full charge will suddenly appear with only half charge. This is because the max fuel will double while the current fuel of the item will stay the same. Once you'll recharge/repair the item, it will go back to full charge and work as intended
+
+In short, don't worry if you suddenly have every item damaged/used. It's just because the "usage meter" of the item displays a percentage based on the maxFuel value
+
+## Credits
+
+Thanks to .M.I.K.E. for the idea
